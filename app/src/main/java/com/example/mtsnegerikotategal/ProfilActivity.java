0343a7e8bbf0c9395bab1849c7b9ad5394ef8610
msyaiful21 <msyaifulazis4@gmail.com>
2021-02-26 10:45:57 +0700

    menambahkan onClick sarpras,tentang dan galeri

--- app/src/main/java/com/example/mtsnegerikotategal/ProfilActivity.java
+++ app/src/main/java/com/example/mtsnegerikotategal/ProfilActivity.java
@@ -2,7 +2,9 @@ package com.example.mtsnegerikotategal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfilActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil2);
+    }
+    public void visimisi(View view) {
+        Intent intent=new Intent(ProfilActivity.this, VisiMisiActivity.class);
+        startActivity(intent);
+    }
+    public void sarpras(View view) {
+        Intent intent=new Intent(ProfilActivity.this, SarprasActivity.class);
+        startActivity(intent);
+    }
+    public void tentang(View view) {
+        Intent intent = new Intent(ProfilActivity.this, TentangActivity.class);
+        startActivity(intent);
+    }
+    public void galeri(View view) {
+        Intent intent = new Intent(ProfilActivity.this, GaleriActivity.class);
+        startActivity(intent);
     }
 }
